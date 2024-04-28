2635dab32f119f31298a02fc276ce4f46cf152e3
Chris McComb <ccmcc2012@gmail.com>
2024-04-28 14:24:21 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import scholarly
+from tqdm.auto import tqdm
+import sentence_transformers
+import pandas
+import datasets
+import sys
+
+# Get author info from Google Scholar
+author = scholarly.scholarly.fill(scholarly.scholarly.search_author_id("0P9w_S0AAAAJ"))
+
+# Load embedding model
+model = sentence_transformers.SentenceTransformer("allenai-specter")
+
+# Declare blank list to append to
+publication_info = []
+
+# Iterate through publications and fill
+for i in tqdm(range(len(author["publications"]))):
+    this_pub = scholarly.scholarly.fill(author["publications"][i])
+    publication_info.append(
+        {
+            **this_pub["bib"],
+            "author_pub_id": this_pub.get("author_pub_id"),
+            "num_citations": this_pub.get("num_citations"),
+            "citedby_url": this_pub.get("citedby_url"),
+            "cites_id": this_pub.get("cites_id"),
+            "pub_url": this_pub.get("pub_url"),
+            "url_related_articles": this_pub.get("url_related_articles"),
+            **this_pub["cites_per_year"],
+            "embedding": model.encode(
+                this_pub["bib"]["title"] + " " + this_pub["bib"]["abstract"]
+            ),
+        }
+    )
+
+# Convert to a dataset and upload to hugginface
+dataset = datasets.Dataset.from_list(publication_info)
+dataset.push_to_hub("ccm/publications", use_auth_token=sys.argv[1])
