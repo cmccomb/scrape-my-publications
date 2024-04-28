f747af7eabb260d724152e6d0633e0297f10f941
Chris McComb <ccmcc2012@gmail.com>
2024-04-28 15:24:17 -0400

    Updating for readability

--- main.py
+++ main.py
@@ -16,33 +16,35 @@ author = scholarly.scholarly.fill(scholarly.scholarly.search_author_id("0P9w_S0A
 model = sentence_transformers.SentenceTransformer("allenai-specter")
 
 # Declare blank list to append to
-publication_info = []
+publication_data = []
 
 # Iterate through publications and fill
 for i in tqdm(range(len(author["publications"]))):
     # Fill the publication info
-    this_pub = scholarly.scholarly.fill(author["publications"][i])
+    publication = scholarly.scholarly.fill(author["publications"][i])
 
     # Embed the publication text
-    vector = model.encode(
-        this_pub["bib"]["title"] + " " + str(this_pub["bib"].get("abstract"))
+    embedding_vector = model.encode(
+        publication["bib"]["title"] + " " + str(publication["bib"].get("abstract"))
     )
 
     # Append data to the list
-    publication_info.append(
+    publication_data.append(
         {
-            **this_pub["bib"],
-            "author_pub_id": this_pub.get("author_pub_id"),
-            "num_citations": this_pub.get("num_citations"),
-            "citedby_url": this_pub.get("citedby_url"),
-            "cites_id": this_pub.get("cites_id"),
-            "pub_url": this_pub.get("pub_url"),
-            "url_related_articles": this_pub.get("url_related_articles"),
-            **this_pub["cites_per_year"],
-            "embedding": vector,
+            **publication["bib"],
+            "author_pub_id": publication.get("author_pub_id"),
+            "num_citations": publication.get("num_citations"),
+            "citedby_url": publication.get("citedby_url"),
+            "cites_id": publication.get("cites_id"),
+            "pub_url": publication.get("pub_url"),
+            "url_related_articles": publication.get("url_related_articles"),
+            **publication["cites_per_year"],
+            "embedding": embedding_vector,
         }
     )
 
 # Convert to a dataset and upload to huggingface. Converting to pandas and then to dataset avoids some weird errors
-dataset = datasets.Dataset.from_pandas(pandas.DataFrame.from_dict(publication_info))
-dataset.push_to_hub("ccm/publications", token=HF_TOKEN)
+publication_dataset = datasets.Dataset.from_pandas(
+    pandas.DataFrame.from_dict(publication_data)
+)
+publication_dataset.push_to_hub("ccm/publications", token=HF_TOKEN)
