1696d6ac83819e3eefafb8240f1628b9bef97baf
Chris McComb <ccmcc2012@gmail.com>
2025-04-13 19:47:27 -0400

    Incremental update

--- main.py
+++ main.py
@@ -101,69 +101,6 @@ for i in tqdm(range(len(new_publications))):
             "embedding": embedding_vector,
         }
     )
-#
-# # Declare blank list to append to
-# publication_data = []
-#
-# # Iterate through publications and fill
-# for i in tqdm(range(len(author["publications"]))):
-#
-#     # Random sleep to avoid rate limiting
-#     time.sleep(random.randint(5, 20))
-#
-#     # Fill the publication info
-#     publication = scholarly.scholarly.fill(author["publications"][i])
-#
-#     # Embed the title and abstract
-#     embedding_vector = (
-#         model(
-#             **tokenizer(
-#                 publication["bib"]["title"]
-#                 + tokenizer.sep_token
-#                 + publication["bib"]["abstract"],
-#                 padding=True,
-#                 truncation=True,
-#                 return_tensors="pt",
-#                 return_token_type_ids=False,
-#                 max_length=512,
-#             )
-#         )
-#         .last_hidden_state[:, 0, :]
-#         .detach()
-#         .numpy()[0]
-#     )
-#
-#     # Update publication info in order to format bibtex well
-#     publication["bib"].update({"pub_type": "article"})
-#     first_authors_last_name = (
-#         author["publications"][i]["bib"]["author"]
-#         .split(" and ")[0]
-#         .split(" ")[-1]
-#         .lower()
-#     )
-#     first_three_words_from_title = "".join(
-#         author["publications"][i]["bib"].get("title", "").split(" ")[:3]
-#     ).lower()
-#     year = str(author["publications"][i]["bib"].get("pub_year", 0))
-#     publication["bib"].update(
-#         {"bib_id": first_authors_last_name + year + first_three_words_from_title}
-#     )
-#
-#     # Append data to the list
-#     publication_data.append(
-#         {
-#             "bibtex": scholarly.scholarly.bibtex(publication),
-#             "bib_dict": publication["bib"],
-#             "author_pub_id": publication.get("author_pub_id"),
-#             "num_citations": publication.get("num_citations"),
-#             "citedby_url": publication.get("citedby_url"),
-#             "cites_id": publication.get("cites_id"),
-#             "pub_url": publication.get("pub_url"),
-#             "url_related_articles": publication.get("url_related_articles"),
-#             **publication["cites_per_year"],
-#             "embedding": embedding_vector,
-#         }
-#     )
 
 # Convert to a dataset. Converting to pandas and then to dataset avoids some weird errors
 new_table = pandas.DataFrame.from_dict(new_publication_data)
